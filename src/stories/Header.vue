296faaa825242994c523433abbf1512a4480c6de
ZhQuella <827265905@qq.com>
2021-04-20 18:34:46 +0800

    Storybook组件展示

---
+++ src/stories/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <div>
+        <h1>Az-UI</h1>
+      </div>
+      <div>
+        <my-button size="small" @click="$emit('logout')" label="Log out" v-if="user" />
+        <my-button size="small" @click="$emit('login')" label="Log in" v-if="!user" />
+        <my-button primary size="small" @click="$emit('createAccount')" label="Sign up" v-if="!user" />
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import './header.css';
+import MyButton from './Button.vue';
+
+export default {
+  name: 'my-header',
+
+  components: { MyButton },
+
+  props: {
+    user: {
+      type: Object,
+    },
+  },
+
+  emits: ['login', 'logout', 'createAccount'],
+};
+</script>
