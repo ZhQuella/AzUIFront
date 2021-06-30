7adad7ea16d66974e79dcf610605fe33252de23d
ZhQuella <827265905@qq.com>
2021-06-30 13:40:02 +0800

    添加Menu组件(未完成)

---
+++ packages/azui/src/packages/Menu/Menu.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    123
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, App } from "vue";
+
+import vptypes from 'vptypes';
+
+const AzMenu = defineComponent({
+  name: "AzMenu",
+  props: {
+    mode: vptypes.oneOfString(['horizontal', 'vertical', 'popper']).def('horizontal')
+  },
+  setup () {
+    return {}
+  }
+})
+
+AzMenu.install = (app: App) => {
+  app.component(AzMenu.name, AzMenu);
+}
+
+export default AzMenu;
+</script>
